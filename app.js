844952d3489c733ad0e6b6e4245edf3f51981c01
Enayat Ali <39117929+enayat-ali@users.noreply.github.com>
2024-12-01 01:31:24 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let boxes = document.querySelector(".box");
+let turn =true;
+
+const winPattern=[
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [2,4,6],
+  [1,4,7],
+  [2,5,8],
+  [3,4,5],
+  [6,7,8],
+  ]
